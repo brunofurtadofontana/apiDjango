27318b05534d7e679f73538e2a09b5ae76afa8b0
Bruno Furtado Fontana <brunofurtado86@hotmail.com>
2020-10-07 21:21:11 -0300

    upload config heroku

--- library/settings.py
+++ library/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+django_heroku.settings(locals())
